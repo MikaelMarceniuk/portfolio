b94f9396bdf899570f07eb862a764900573afbec
MikaelMarceniuk <mika.marceniuk@gmail.com>
2026-06-17 05:39:43 -0300

    feat(cvs): add English and Portuguese CV files and update links

--- src/constants/cv-links.constants.ts
+++ src/constants/cv-links.constants.ts
@@ -1,5 +1,5 @@
 export const CV_LINKS = [
-  { label: 'CV — PT-BR', file: '/cv/mikael-marceniuk-pt.pdf' },
+  { label: 'CV — PT-BR', file: '/cv/mikael-marceniuk-pt-br.pdf' },
   { label: 'CV — EN (US)', file: '/cv/mikael-marceniuk-en-us.pdf' },
-  { label: 'CV — EN (EU)', file: '/cv/mikael-marceniuk-en-eu.pdf' },
+  // { label: 'CV — EN (EU)', file: '/cv/mikael-marceniuk-en-eu.pdf' },
 ]
