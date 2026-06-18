f7fa541622031fbccfa925f052f6b1bb2655db94
MikaelMarceniuk <mika.marceniuk@gmail.com>
2026-06-18 17:12:51 -0300

    feat: add proxy function for locale management based on Cloudflare headers

---
+++ src/proxy.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function proxy(request: NextRequest) {
+  const response = NextResponse.next()
+
+  const existingLocale = request.cookies.get('NEXT_LOCALE')
+  if (existingLocale) return response
+
+  // Pega o país pelo header da Cloudflare
+  const country = request.headers.get('CF-IPCountry')
+  console.log('proxy.country: ', country)
+  const locale = country === 'BR' ? 'pt-br' : 'en-us'
+
+  response.cookies.set('NEXT_LOCALE', locale, {
+    path: '/',
+    maxAge: 60 * 60 * 24 * 365, // 1 ano
+    sameSite: 'lax',
+  })
+
+  return response
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)'],
+}
